78473f6cb0f478e2ad18a2191d473180a2598067
Aleksei <flipis4e@yandex.ru>
2025-01-22 16:07:21 +0300

    правка base в конфиге

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/for-lesta-games/',
   plugins: [react()],
   server: {
     port: 3000,
